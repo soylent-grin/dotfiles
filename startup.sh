f413f5a63a2a32e730250cf4bdc2d492389ef317
soylent-grin <hocico16@gmail.com>
2015-12-13 23:27:04 +0300

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,9 @@
+ #!/bin/bash
+ 
+ sudo apt-get update && sudo apt-get install git
+ 
+ git config --global user.name "soylent-grin"
+ 
+ git clone git@github.com:soylent-grin/dotfiles.git && cd ./dotfiles
+ 
+ bash ./init.sh
