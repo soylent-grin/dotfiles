e271e520c349ab8a9c4f7cc4249978e020565f3c
soylent-grin <hocico16@gmail.com>
2015-12-13 23:12:12 +0300

    Update install-apps.sh

--- scripts/install-apps.sh
+++ scripts/install-apps.sh
@@ -1,36 +1,61 @@
 #!/bin/bash
 
+REPOS=(
+  "ppa:me-davidsansome/clementine"
+  "ppa:webupd8team/sublime-text-3"
+)
+
 APPS=(
+  sublime-text-installer
+  clementine
   guake 
   git 
-  maven python-pip httpie nginx 
+  maven 
+  python-pip 
+  httpie 
+  nginx
+  vlc
+  transmission
+  skype
   openssh-server
 )
 
+NODE_MODULES=(
+  nodemon
+  n
+  eslint 
+  grunt-cli 
+  trash 
+  vtop 
+  ttystudio 
+  node-inspector
+)
+
 echo "installing apps..."
+
+for repo in "${REPOS[@]}"
+do
+  sudo add-apt-repository -y ${repo}
+done
+
+sudo apt-get update
+
+for app in "${APPS[@]}"
+do
+  sudo apt-get install -y ${app} 
+done
+
+for module in "${NODE_MODULES[@]}"
+do
+  sudo npm install -g ${module} 
+done
+
+# thefuck
+sudo pip install thefuck
 
 # chrome
 sudo apt-get install -y libxss1 libappindicator1 libindicator7
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome*.deb
-
-# clementine
-sudo add-apt-repository -y ppa:me-davidsansome/clementine
-sudo apt-get update
-sudo apt-get install -y clementine
-
-# sublime
-sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
-sudo apt-get update
-sudo apt-get install sublime-text-installer
-
-# other
-sudo apt-get install -y guake git maven python-pip httpie nginx openssh-server
-
-# node modules
-sudo npm i -g n nodemon eslint grunt-cli trash vtop ttystudio node-inspector
-
-# thefuck
-sudo pip install thefuck
 
 echo "done;"
